996cafa357a8a5b51f937309c6fcc3c25d751dfb
Maiker260 <gutierrezmaiker@gmail.com>
2024-05-27 20:33:23 -0600

    Add Task and Project Modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const showModalTask = document.querySelector("#add_new_todo_task_dialog");
+const showModalProject = document.querySelector("#add_new_project_dialog");
+const newTaskBtn = document.querySelector("#add_new_todo_task_btn");
+const newProjectBtn = document.querySelector("#add_new_todo_project_btn");
+
+newTaskBtn.addEventListener("click", () => {
+    showModalTask.showModal();
+});
+newProjectBtn.addEventListener("click", () => {
+    showModalProject.showModal();
+});
